4fab3bee162f9b7d518f6e154b2132dc8ea3a78a
Edwin A. Bermudez Ardila <edwinbermudez117@gmail.com>
2021-11-22 22:11:43 -0500

    Listado de Sitios de interes desde JSON

---
+++ app/src/main/java/co/com/mintic/talleres/Points.kt
@@ -0,0 +1,11 @@
+package co.com.mintic.talleres
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Points : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_points)
+    }
+}
---
+++ app/src/main/java/co/com/mintic/talleres/WorkshopAdapter.kt
@@ -0,0 +1,69 @@
+package co.com.mintic.talleres
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+import com.bumptech.glide.Glide
+
+
+class WorkshopAdapter (
+    private val mWorkshops: ArrayList<Workshops>,
+    private val context: Context,
+    private val onClick:(Workshops?) -> Unit
+) : RecyclerView.Adapter<WorkshopAdapter.WorkshopViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkshopViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.workshops_list_item, parent, false)
+        return WorkshopViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holderContact: WorkshopViewHolder, position: Int) {
+        val workshop = mWorkshops[position]
+        holderContact.bind(workshop = workshop)
+    }
+
+
+    override fun getItemCount(): Int {
+        return mWorkshops.size
+    }
+
+    inner class WorkshopViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var titleLabel: TextView = itemView.findViewById(R.id.textview_title)
+        private var descLabel: TextView = itemView.findViewById(R.id.textview_desc)
+        private var imageView: ImageView = itemView.findViewById(R.id.imageView)
+        private var ratingBar2: RatingBar = itemView.findViewById(R.id.ratingBar_rating)
+        private var currentWorkshop:Workshops? = null
+        init {
+            itemView.setOnClickListener {
+                Log.d(TAG,"itemView OnClick, contacto: ${currentWorkshop}")
+                onClick(currentWorkshop)
+            }
+        }
+
+        fun bind(workshop: Workshops) {
+            currentWorkshop= workshop
+            titleLabel.text = workshop.title
+            descLabel.text = workshop.desc
+            Log.d(TAG,workshop.imageUrl)
+            Glide.with(context)
+                .load(workshop.imageUrl)
+                .into(imageView)
+
+
+        }
+
+    }
+
+
+    companion object{
+        private const val TAG = "WorkshopAdapter"
+    }
+}
